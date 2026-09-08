c2d501652966fb212db6d42c88c478f4dc103375
liyiming <li1361154598@163.com>
2026-09-08 08:56:27 +0800

    todo：115 & 940 hard

---
+++ cpp-template/leetcode/editor/cn/dynamic-programming/115.hard.distinct-subsequences.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=115 lang=cpp
+ * @lcpr version=30404
+ *
+ * [115] 不同的子序列
+ */
+
+/*
+ * [115] 不同的子序列
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../../common/ListNode.cpp"
+#include "../../common/TreeNode.cpp"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        
+    }
+};
+// @lc code=end
+
+int main() {
+    Solution solution;
+    // your test code here
+}
+
+
+
+/*
+// @lcpr case=start
+// "rabbbit"\n"rabbit"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "babgbag"\n"bag"\n
+// @lcpr case=end
+
+ */
+
---
+++ cpp-template/leetcode/editor/cn/dynamic-programming/940.hard.distinct-subsequences-ii.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=940 lang=cpp
+ * @lcpr version=30404
+ *
+ * [940] 不同的子序列 II
+ */
+
+/*
+ * [977] 不同的子序列 II
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../../common/ListNode.cpp"
+#include "../../common/TreeNode.cpp"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        
+    }
+};
+// @lc code=end
+
+int main() {
+    Solution solution;
+    // your test code here
+}
+
+
+
+/*
+// @lcpr case=start
+// "abc"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "aba"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "aaa"\n
+// @lcpr case=end
+
+ */
+
