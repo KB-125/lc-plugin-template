193c6f775a1f3f1c3035ddee78ac7e93b46573e4
liyiming <li1361154598@163.com>
2026-09-09 13:48:21 +0800

    daily：3871，better solution

--- cpp-template/leetcode/editor/cn/Unknown/3871.medium.count-commas-in-range-ii.cpp
+++ cpp-template/leetcode/editor/cn/Unknown/3871.medium.count-commas-in-range-ii.cpp
@@ -20,7 +20,7 @@ using namespace std;
 class Solution {
 public:
     long long countCommas(long long n) {
-        long long res = 0, t =0;
+        /* long long res = 0, t =0;
         vector<unsigned int> mods;
         mods.reserve(6);
         for (long long m = n % 1000 , k = n / 1000, i = 1; k > 0; m = k % 1000 , k/=1000 ,++i) {
@@ -46,6 +46,19 @@ public:
 
         
         //res -= (size) * std::pow(1000,size);
+        return res; */
+
+        /*
+        大于等于 1000的数，都会包含至少 1 个逗号，这一部分总共贡献 n−1000+1 个逗号。
+        大于等于 1000000的数，会包含至少 2 个逗号（即在前一步的基础上，每个数额外多贡献 1 个逗号），总共额外贡献 n−1000000+1 个。
+        以此类推，我们只需通过变量 p 每次乘以 1000，并累加当前段的贡献值 n−p+1 即可。
+
+        */
+        long long p = 1000, res = 0;
+        while (p <= n) {
+            res += n - p + 1;
+            p *= 1000;
+        }
         return res;
     }
 };
