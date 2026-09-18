2fe4e952c5f120c33d1a54fd27da83efd04bdaa0
liyiming <li1361154598@163.com>
2026-09-18 10:51:36 +0800

    daily: 1520 todo

---
+++ cpp-template/leetcode/editor/cn/greedy/1520.hard.maximum-number-of-non-overlapping-substrings.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=1520 lang=cpp
+ * @lcpr version=30404
+ *
+ * [1520] 最多的不重叠子字符串
+ */
+
+/*
+ * [1644] 最多的不重叠子字符串
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../../common/ListNode.cpp"
+#include "../../common/TreeNode.cpp"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> maxNumOfSubstrings(string s) {
+        //TODO:
+    }
+};
+// @lc code=end
+
+int main() {
+    Solution solution;
+    // your test code here
+}
+
+
+
+/*
+// @lcpr case=start
+// "adefaddaccc"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abbaccd"\n
+// @lcpr case=end
+
+ */
+
