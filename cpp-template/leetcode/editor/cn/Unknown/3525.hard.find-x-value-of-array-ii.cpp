017e43ebac77d7f0d5660bbbf5b5ff261651e0f2
liyiming <li1361154598@163.com>
2026-09-22 11:36:53 +0800

    daily:3525 todo

---
+++ cpp-template/leetcode/editor/cn/Unknown/3525.hard.find-x-value-of-array-ii.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=3525 lang=cpp
+ * @lcpr version=30404
+ *
+ * [3525] 求出数组的 X 值 II
+ */
+
+/*
+ * [3840] 求出数组的 X 值 II
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../../common/ListNode.cpp"
+#include "../../common/TreeNode.cpp"
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums, int k, vector<vector<int>>& queries) {
+        //TODO: busy now
+    }
+};
+// @lc code=end
+
+int main() {
+    Solution solution;
+    // your test code here
+}
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n3\n[[2,2,0,2],[3,3,3,0],[0,1,0,1]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,4,8,16,32]\n4\n[[0,2,0,2],[0,2,0,1]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,1,2,1,1]\n2\n[[2,1,0,1]]\n
+// @lcpr case=end
+
+ */
+
